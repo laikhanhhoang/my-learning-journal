e1cec1681a48306f491ec985af17dedce41965ca
Lai Khanh Hoang <laikhanhhoang3011@gmail.com>
2025-12-09 20:26:50 +0700

    Update generate_readme.py

--- generate_readme.py
+++ generate_readme.py
@@ -3,18 +3,14 @@ import os
 course_folder = "courses"
 
 with open("README.md", "w", encoding="utf-8") as readme:
-    
-    header = "# All My Courses \n"
-    readme.write(header) # readme là biến được mở và đang hoạt động
+    readme.write("# All My Courses\n\n")
 
     for filename in sorted(os.listdir(course_folder)):
-        
         if filename.endswith(".md"):
-            
             filepath = os.path.join(course_folder, filename)
-            
-            with open(filepath, "r", encoding="utf-8") as f: # Chế độ 'r' để đọc
+
+            with open(filepath, "r", encoding="utf-8") as f:
                 content = f.read()
-            
-            readme.write(content + "\n\n") # Ghi nội dung và thêm dòng mới
-            
+
+            # Chỉ ghi nội dung, KHÔNG ghi tên file
+            readme.write(content + "\n\n")
